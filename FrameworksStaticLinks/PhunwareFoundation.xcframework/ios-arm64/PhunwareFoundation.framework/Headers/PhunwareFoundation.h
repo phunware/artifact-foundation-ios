a092b8b8cc27e62b68401c5008151ca991f66344
jenkins <hostmaster@phunware.com>
2025-04-17 21:01:44 -0700

    Update version 1.1.0. See CHANGELOG for details.

---
+++ FrameworksStaticLinks/PhunwareFoundation.xcframework/ios-arm64/PhunwareFoundation.framework/Headers/PhunwareFoundation.h
@@ -0,0 +1,19 @@
+//
+//  PhunwareFoundation.h
+//  PhunwareFoundation
+//
+//  Created by Troy Stump on 9/28/20.
+//  Copyright © 2020 Phunware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhunwareFoundation.
+FOUNDATION_EXPORT double PhunwareFoundationVersionNumber;
+
+//! Project version string for PhunwareFoundation.
+FOUNDATION_EXPORT const unsigned char PhunwareFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhunwareFoundation/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "PhunwareFoundation",
+    platforms: [
+        .iOS("15.5")
+    ],
+    products: [
+        // Only expose these to clients
+        .library(
+            name: "PhunwareFoundation", 
+            targets: ["PhunwareFoundationTargets"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/phunware/maas-core-ios-sdk.git", from: "3.13.0"),
+    ],
+    targets: [
+        // Internal frameworks
+        .binaryTarget(
+            name: "PhunwareFoundation",
+            path: "FrameworksStaticLinks/PhunwareFoundation.xcframework"
+        ),
+        .target(name: "PhunwareFoundationTargets",
+            dependencies:[
+                .target(name: "PhunwareFoundation"),
+                .product(name: "PWCore", package: "maas-core-ios-sdk", condition: nil),
+                .product(name: "DeviceIdentity", package: "maas-core-ios-sdk", condition: nil),
+            ],
+            path: "PhunwareFoundationTargets"
+        )
+    ]
+)
